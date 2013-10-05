2d7d1699146f548cbb9244dd9ffba4169f3496b0
ken endo <ken223@gmail.com>
2013-10-06 00:27:32 +0900

    1st commit

--- exec.sh
+++ exec.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 HADOOP="/Users/endou/tool/cdh/hadoop-2.0.0-cdh4.4.0/bin/hadoop"
+HDFS_ROOT="hdfs://192.168.56.102:8020"
 
-${HADOOP} jar work/MapReduceSampleCdh4.jar:lib/* com.example.hadoop.MapReduceSampleCdh4 /user/endo/sample/input /user/endo/sample/output 
+hdfs dfs -rm -r ${HDFS_ROOT}/user/endo/sample/output 
+export HADOOP_CLASSPATH=conf
+
+${HADOOP} jar ./work/MapReduceSampleCdh4.jar com.example.hadoop.MapReduceSampleCdh4 ${HDFS_ROOT}/user/endo/sample/input ${HDFS_ROOT}/user/endo/sample/output 
